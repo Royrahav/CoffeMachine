1da63f1c689192dee23453e57c8b3d90680488b5
Royrahav <roy.r770@gmail.com>
2022-06-30 22:32:30 +0300

    Added some documentation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ money_amount = 0
 
 
 def calculate_price(money_map):
+    """ Calculates the total price according to inserted money_map (coin -> value). """
     amount = 0
     for x in money_map:
         amount += money_map[x][0] * money_map[x][1]
@@ -11,6 +12,7 @@ def calculate_price(money_map):
 
 
 def process_coins():
+    """ Calculates inserted coins value. """
     money_map = {
         "quarters": [int(input("How many quarters ($0.25)? ")), 0.25],
         "dimes": [int(input("How many dimes ($0.1)? ")), 0.1],
@@ -22,6 +24,10 @@ def process_coins():
 
 
 def check_resources(drink):
+    """
+    Checks if there's enough resources for a drink in the machine.
+    Returns False if we're out of resources
+    """
     ingredients = mr.MENU[drink]["ingredients"]
     for x in mr.resources:
         if x in ingredients:
@@ -32,6 +38,7 @@ def check_resources(drink):
 
 
 def report():
+    """ Gets info about ingredients status and profits inside the machine. """
     measurement = ""
     for x in mr.resources:
         if x.lower() == "water" or x.lower() == "milk":
@@ -44,11 +51,16 @@ def report():
 
 
 def turn_off():
+    """ Turns the machine off. """
     print("Bye!")
     return False
 
 
 def calculate_change_and_price(drink):
+    """
+    Checks if entered coins are sufficient, returns False if not.
+    Also, it calculates the change
+    """
     global money_amount
     cost = mr.MENU[drink]["cost"]
     print(f"{drink} costs &{cost}. How would you like to pay?")
@@ -66,6 +78,10 @@ def calculate_change_and_price(drink):
 
 
 def validate_drink(drink):
+    """
+    Checks if selected drink has enough resources and user entered enough coins.
+    Returns True if all conditions are met, and False if one of them isn't.
+    """
     if not check_resources(drink):
         return False
     if not calculate_change_and_price(drink):
@@ -74,18 +90,21 @@ def validate_drink(drink):
 
 
 def is_drink(drink):
+    """ Checks if the entered value is a drink, returns True, or another command, returns False. """
     if drink in mr.MENU:
         return True
     return False
 
 
 def update_ingredients(drink):
+    """ Updates ingredients map after making a particular drink. """
     ingredients = mr.MENU[drink]["ingredients"]
     for x in ingredients:
         mr.resources[x] -= ingredients[x]
 
 
 def machine_on():
+    """ Main machine operation func. """
     choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
     if is_drink(choice):
